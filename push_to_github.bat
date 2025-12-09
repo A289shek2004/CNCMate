9d60b0a583b51a7620eca3220e5ca0f754642cc9
A289shek2004 <abhishekgup2004@gmail.com>
2025-12-09 16:59:17 +0530

    Final submission of CNCMate Project

---
+++ push_to_github.bat
@@ -0,0 +1,9 @@
+# Push to GitHub Script
+git init
+git add .
+git commit -m "Final submission of CNCMate Project"
+git branch -M main
+git remote add origin https://github.com/A289shek2004/CNCMate
+git remote set-url origin https://github.com/A289shek2004/CNCMate
+git push -u origin main
+Pause
